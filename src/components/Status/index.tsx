f88cc46cba78ac7746fecbcc81e48f6af1c14218
Debi7 <debi1@mail.ru>
2024-07-11 21:03:31 +0300

    emoji

--- src/components/Status/index.tsx
+++ src/components/Status/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 
+const emojiImage = "😊";
 interface StatusProps {
   start: boolean;
   setStart: (value: boolean) => void;
@@ -7,11 +8,15 @@ interface StatusProps {
 
 const Status: FC<StatusProps> = ({ start, setStart }) => (
   <>
-    <h1>
-      {typeof start === "undefined"
-        ? `⏱ Цель игры, как можно быстрее напечатать буквы и знаки, кроме пробелов, чтобы уложиться в таймер.`
-        : `Попробуйте еще раз`}
-    </h1>
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <h1>
+        {typeof start === "undefined"
+          ? `⏱ Цель игры, как можно быстрее напечатать буквы и знаки, кроме пробелов, чтобы уложиться в таймер.`
+          : <>
+            <span style={{ fontSize: "32px", marginRight: "8px", fontStyle: "normal" }}>{emojiImage}</span> Попробуйте еще раз
+          </>}
+      </h1>
+    </div>
     <div className="start-btn-wrapper">
       <button onClick={() => setStart(true)}>Старт</button>
     </div>
